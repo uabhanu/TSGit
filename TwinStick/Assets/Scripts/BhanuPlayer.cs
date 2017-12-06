35fddfe31d83447c73aef0203d5dde00eed05d87
Bhanu <uabhanu@gmail.com>
2017-12-06 13:11:10 +0000

    Section 10 Lecture 272 WIP

---
+++ TwinStick/Assets/Scripts/BhanuPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class BhanuPlayer : MonoBehaviour 
+{
+
+	void Start() 
+    {
+		
+	}
+
+	void Update() 
+    {
+		if(Time.timeScale == 0)
+            return;
+
+        Debug.Log("H : " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("V : " + CrossPlatformInputManager.GetAxis("Vertical"));
+	}
+}
