3d05492f4329e5b06427acf67f02ca0eb677db8f
Bhanu <uabhanu@gmail.com>
2017-12-06 15:48:55 +0000

    Section 10 Lecture 275 Up Next

---
+++ TwinStick/Assets/Scripts/BhanuReplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BhanuReplay : MonoBehaviour 
+{
+
+	void Start() 
+    {
+		
+	}
+
+	void Update() 
+    {
+		
+	}
+}
+
+public class BhanuKeyframe
+{
+    float m_frameTime;
+    Quaternion m_rotation;
+    Vector3 m_position;
+
+    public BhanuKeyframe(float time , Quaternion rot , Vector3 pos)
+    {
+        m_frameTime = time;
+        m_rotation = rot;
+        m_position = pos;
+    }
+}
