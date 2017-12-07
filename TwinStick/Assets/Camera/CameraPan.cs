49a369a170885ee24ff88077d3d20911d5ce9e60
Bhanu <uabhanu@gmail.com>
2017-12-07 16:03:49 +0000

    Section 10 Finished

---
+++ TwinStick/Assets/Camera/CameraPan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour 
+{
+    GameObject m_bhanuPlayer;
+
+	void Start() 
+    {
+		m_bhanuPlayer = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void LateUpdate() 
+    {
+		if(Time.timeScale == 0)
+            return;
+
+            transform.LookAt(m_bhanuPlayer.transform);
+	}
+}
---
+++ TwinStick/Assets/Camera/SelfieStick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfieStick : MonoBehaviour 
+{
+    float m_panSpeed = 1.95f;
+    GameObject m_bhanuPlayer;
+    Vector3 m_armRotation;
+
+	void Start() 
+    {
+        m_armRotation = transform.rotation.eulerAngles;
+        m_bhanuPlayer = GameObject.FindGameObjectWithTag("Player");	
+	}
+
+	void Update() 
+    {
+		if(Time.timeScale == 0)
+            return;
+
+        m_armRotation.x += Input.GetAxis("RVertical") * m_panSpeed;
+        m_armRotation.y += Input.GetAxis("RHorizontal") * m_panSpeed;
+        transform.position = m_bhanuPlayer.transform.position;
+        transform.rotation = Quaternion.Euler(m_armRotation);
+	}
+}
